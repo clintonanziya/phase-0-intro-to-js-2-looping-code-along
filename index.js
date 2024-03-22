8bd0495e9b29905621d075c44205272ba639955b
clintonanziya <clintonanziya3@gmail.com>
2024-03-22 15:25:51 +0300

    completed-phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
